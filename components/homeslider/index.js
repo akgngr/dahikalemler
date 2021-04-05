ba1c5b250e1b6a094d4e0729b082fb61368b0005
akgngr <abdulkadirgngr@hotmail.com>
2021-04-05 22:34:50 +0300

    fix image upload 7

--- components/homeslider/index.js
+++ components/homeslider/index.js
@@ -2,63 +2,78 @@ import SwiperCore, { Navigation, Pagination, Autoplay, A11y, Lazy, EffectFade } 
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles.module.css'
+import { attributes, react as SliderContent } from '../content/slider.md';
 
 SwiperCore.use([Navigation, Pagination, Autoplay, A11y, Lazy, EffectFade]);
 
-const Slider = () => {
-  return (
-    <Swiper
-      navigation
-      className={styles.slidecontainer}
-      slidesPerView={1}
-      pagination={{ clickable: true }}
-      autoplay={true}
-      lazy={true}
-      effect={"fade"}
-      loop={true}
-    >
-      <SwiperSlide>
-        <div className="">
-          <div className="bgWrap">
-            <img
-              alt="Mountains"
-              src="/image/slider/bg_1.jpg"
-            />
-          </div>
-          <div className={styles.overlay}></div>
-          <div className={styles.textcontainer}>
-            <div className={styles.slidertext}>
-              <div className={styles.text}>
-                <h1 className={styles.texthead}>Education Needs Complete Solution</h1>
-                <p className={styles.textp}>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                <p className={styles.textp}><a href="#" className={styles.textbutton + ' button-default'} >Contact Us</a></p>
+export default class Slider extends Component {
+  
+  render(){
+    let { slider } = attributes;
+    return (
+      <>
+      <Swiper
+        navigation
+        className={styles.slidecontainer}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={true}
+        lazy={true}
+        effect={"fade"}
+        loop={true}
+      >
+        <SwiperSlide>
+          <div className="">
+            <div className="bgWrap">
+              <img
+                alt="Mountains"
+                src="/image/slider/bg_1.jpg"
+              />
+            </div>
+            <div className={styles.overlay}></div>
+            <div className={styles.textcontainer}>
+              <div className={styles.slidertext}>
+                <div className={styles.text}>
+                  <h1 className={styles.texthead}>Education Needs Complete Solution</h1>
+                  <p className={styles.textp}>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p className={styles.textp}><a href="#" className={styles.textbutton + ' button-default'} >Contact Us</a></p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider-item">
-          <div className="bgWrap">
-            <img
-              alt="Mountains"
-              src="/image/slider/bg_2.jpg"
-            />
-          </div>
-          <div className={styles.overlay}></div>
-          <div className={styles.textcontainer}>
-            <div className={styles.slidertext}>
-              <div className={styles.text}>
-                <h1 className={styles.texthead}>Education Needs Complete Solution</h1>
-                <p className={styles.textp}>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                <p className={styles.textp}><a href="#" className={styles.textbutton + ' button-default'} >Contact Us</a></p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slider-item">
+            <div className="bgWrap">
+              <img
+                alt="Mountains"
+                src="/image/slider/bg_2.jpg"
+              />
+            </div>
+            <div className={styles.overlay}></div>
+            <div className={styles.textcontainer}>
+              <div className={styles.slidertext}>
+                <div className={styles.text}>
+                  <h1 className={styles.texthead}>Education Needs Complete Solution</h1>
+                  <p className={styles.textp}>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p className={styles.textp}><a href="#" className={styles.textbutton + ' button-default'} >Contact Us</a></p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  );
+        </SwiperSlide>
+      </Swiper>
+      <ul>
+      {cats.map((slider, k) => (
+        <li key={k}>
+          <h2>{slider.name}</h2>
+          <p>{slider.description}</p>
+        </li>
+      ))}
+    </ul>
+    </>
+    );
+  }
 }
 
 export default Slider
