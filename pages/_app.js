8aee3aa8e69b763963816819eaac91dd3d659dce
akgngr <abdulkadirgngr@hotmail.com>
2021-04-06 10:33:20 +0300

    fix image upload

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,8 @@ import 'swiper/components/navigation/navigation.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 import 'swiper/components/scrollbar/scrollbar.min.css';
 import 'swiper/components/effect-fade/effect-fade.min.css';
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
